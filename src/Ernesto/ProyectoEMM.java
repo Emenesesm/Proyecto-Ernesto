ae7cc1aece9b417eaca36e11328e2f739d9e1aef
Ernesto Meneses <emenesesm2018@gmail.com>
2022-09-06 22:19:09 +0200

    implementacion del metodo mayor valor

--- src/Ernesto/ProyectoEMM.java
+++ src/Ernesto/ProyectoEMM.java
@@ -3,8 +3,21 @@ package Ernesto;
 public class ProyectoEMM {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numMayor = 0;
+		int numMenor = 0;
+		int[] numero = new int[100];
+		for (int x = 0; x < numero.length; x++) {
 
+			numero[x] = (int) (Math.random() * 100);
+			System.out.println(numero[x]);
+
+		}
+		for (int valor : numero) {
+			if (numMayor < valor) {
+				numMayor = valor;
+			}
+		}
+		System.out.println("El numero mayor es " + numMayor);
 	}
 
 }
